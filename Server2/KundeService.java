07848343ea9abcd1b0dcbe87d832b9cc45c0f215
StamosUltra <joaki.m@hotmail.com>
2017-09-06 08:25:47 +0200

    First commit

---
+++ Server2/KundeService.java
@@ -0,0 +1,23 @@
+@Path("/kunder/")
+public class KundeService {
+	private static Map<String,Kunde> kunder = new HashMap<>();
+
+    @GET
+    @Path("/{kundeId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Kunde getKunde(@PathParam("kundeId") String kundeId) {
+        return kunder.get(kundeId);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Kunde> getKunder() {
+        return kunder.values();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addKunde(Kunde kunde) {
+        kunder.put(kunde.getId(), kunde);
+    }
+}
