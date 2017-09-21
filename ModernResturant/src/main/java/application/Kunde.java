b44040603b9e4f17089971f8c95e60e7f6dfef54
StamosUltra <joaki.m@hotmail.com>
2017-09-21 10:43:45 +0200

    First commit

--- ModernResturant/src/main/java/application/Kunde.java
+++ ModernResturant/src/main/java/application/Kunde.java
@@ -1,3 +1,5 @@
+package application;
+
 /**
  * Created by joaki on 12.09.2017.
  */
@@ -12,12 +14,12 @@ public class Kunde {
         return navn;
     }
 
-    public String getId() {
-        return id;
-    }
-
     public void setNavn(String navn) {
         this.navn = navn;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public void setId(String id) {
